2ef4e93aad72ac2e081a3378b1b4fb016e8f518b
WilkerSebastian <wilkerafonso2005@gmail.com>
2025-11-17 12:30:22 -0400

    hotfix: password type

--- src/application/use-cases/Room/create-room.case.ts
+++ src/application/use-cases/Room/create-room.case.ts
@@ -34,7 +34,7 @@ export const createRoom = async(roomDTO: CreateRoomDTO) => {
 
     const playerId = playerOrm.save(player)
 
-    const room = new Room(roomDTO.userID, roomDTO.password)
+    const room = new Room(roomDTO.userID, roomDTO.password!)
 
     roomOrm.save(room)
 
